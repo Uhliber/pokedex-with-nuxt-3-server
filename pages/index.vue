e95d68ccfb4364600f3414d2af34a72f1574b20c
Jess Roque <68854057+jessdolt@users.noreply.github.com>
2023-10-24 15:02:30 +0800

    isolate util functions

--- pages/index.vue
+++ pages/index.vue
@@ -158,16 +158,6 @@ onMounted(async () => {
   searchRandom()
 })
 
-const getRandomNumber = () => {
-  const range1 = Math.floor(Math.random() * (1017 - 1 + 1)) + 1 // Random number between 1 and 1000
-  const range2 = Math.floor(Math.random() * (10292 - 10001 + 1)) + 10000 // Random number between 10000 and 10227
-
-  // Randomly choose between the two ranges
-  const isRange1 = Math.random() < 0.9
-
-  return isRange1 ? range1 : range2
-}
-
 const searchRandom = async () => {
   const randomPokemonNumber = getRandomNumber()
   pokemonInput.value = randomPokemonNumber
@@ -191,11 +181,5 @@ const searchPokemon = async () => {
 
     isSearching.value = false
   }, 500)
-}
-
-const transformInput = (input) => {
-  const lowercased = input.toLowerCase()
-  const kebabCased = lowercased.replace(/[^a-z0-9]+/g, "-")
-  return kebabCased.replace(/^-+|-+$/g, "")
 }
 </script>
