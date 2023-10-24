049093ab57b29e119a255bbac8076e01577aaeea
Uhliber <olvrcp@gmail.com>
2023-10-24 14:43:06 +0800

    add linting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,74 +1,74 @@
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [],
-    theme: {
-      extend: {
-        colors: {
-          pokered: {
-            DEFAULT: colors.red["500"],
-            light: colors.red["300"],
-            dark: colors.red["900"],
-          },
-          pokeblue: {
-            DEFAULT: colors.sky["500"],
-            light: colors.sky["300"],
-            dark: colors.sky["900"],
-          },
-          pokeyellow: {
-            DEFAULT: colors.yellow["500"],
-            light: colors.yellow["300"],
-            dark: colors.amber["700"],
-          },
-          pokegreen: {
-            DEFAULT: colors.lime["500"],
-            light: colors.lime["400"],
-            dark: colors.lime["800"],
-          },
-          pokeblack: {
-            DEFAULT: colors.stone["900"],
-            light: colors.stone["600"],
-            dark: colors.stone["800"],
-          },
-          pokebrown: {
-            DEFAULT: colors.orange["700"],
-            light: colors.orange["600"],
-            dark: colors.orange["800"],
-          },
-          pokepurple: {
-            DEFAULT: colors.purple["500"],
-            light: colors.purple["300"],
-            dark: colors.purple["900"],
-          },
-          pokegray: {
-            DEFAULT: colors.neutral["500"],
-            light: colors.neutral["300"],
-            dark: colors.neutral["800"],
-          },
-          pokewhite: {
-            DEFAULT: colors.stone["100"],
-            light: colors.white,
-            dark: colors.stone["200"],
-          },
-          pokepink: {
-            DEFAULT: colors.pink["500"],
-            light: colors.pink["300"],
-            dark: colors.pink["900"],
-          },
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        pokered: {
+          DEFAULT: colors.red['500'],
+          light: colors.red['300'],
+          dark: colors.red['900'],
         },
-        dropShadow: {
-          light: '8px 8px rgba(226, 232, 240, 0.4)',
+        pokeblue: {
+          DEFAULT: colors.sky['500'],
+          light: colors.sky['300'],
+          dark: colors.sky['900'],
         },
-        backgroundSize: {
-          'size-200': '200% 200%',
+        pokeyellow: {
+          DEFAULT: colors.yellow['500'],
+          light: colors.yellow['300'],
+          dark: colors.amber['700'],
         },
-        backgroundPosition: {
-          'pos-0': '0% 0%',
-          'pos-100': '100% 100%',
+        pokegreen: {
+          DEFAULT: colors.lime['500'],
+          light: colors.lime['400'],
+          dark: colors.lime['800'],
+        },
+        pokeblack: {
+          DEFAULT: colors.stone['900'],
+          light: colors.stone['600'],
+          dark: colors.stone['800'],
+        },
+        pokebrown: {
+          DEFAULT: colors.orange['700'],
+          light: colors.orange['600'],
+          dark: colors.orange['800'],
+        },
+        pokepurple: {
+          DEFAULT: colors.purple['500'],
+          light: colors.purple['300'],
+          dark: colors.purple['900'],
+        },
+        pokegray: {
+          DEFAULT: colors.neutral['500'],
+          light: colors.neutral['300'],
+          dark: colors.neutral['800'],
+        },
+        pokewhite: {
+          DEFAULT: colors.stone['100'],
+          light: colors.white,
+          dark: colors.stone['200'],
+        },
+        pokepink: {
+          DEFAULT: colors.pink['500'],
+          light: colors.pink['300'],
+          dark: colors.pink['900'],
         },
       },
+      dropShadow: {
+        light: '8px 8px rgba(226, 232, 240, 0.4)',
+      },
+      backgroundSize: {
+        'size-200': '200% 200%',
+      },
+      backgroundPosition: {
+        'pos-0': '0% 0%',
+        'pos-100': '100% 100%',
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
   
